e62ac32cc060f2bc5ee19d5bd883328f585b7600
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-11-13 11:45:13 +0700

    sua

--- math/compare.py
+++ math/compare.py
@@ -1,27 +1,29 @@
+from analysis import *
+
 def normal():
-    if 0.8*Pc < Pmax < Pc and 0.8*P_in < P_inxylanh < P_in:
+    if 0.8*Pc < Pmax < Pc and 0.8*P_intake < P_inxylanh < P_intake:
         value = 'Bình thường'
     else:
         value = 'Hư hỏng'
     return value
 
 def damage():
-    if Pci < Pmin:
+    if Pci < P_min:
         damage_c ='Gãy xéc măng, gãy xupap hay bị lủng piston'
-    elif Pmin < Pci <0.62*Pmax:
+    elif P_min < Pci <0.62*Pmax:
         damage_c = 'Hở gioăng nắp máy'
-    elif 0.62*Pmax < Pci <0.8*Pmax:
+    elif 0.62*Pc < Pci <0.8*Pc:
         damage_c = 'Hở xupap'
-    elif Pci > Pmax:
+    elif Pci > Pc:
         damage_c = 'Buồng đốt bị bám mụi than, kẹt xupap xả'
-    elif Pci > 0.9*Pmax:
+    elif Pci > 0.9*Pc:
         damage_c = 'Các xy lanh mòn không đều'
     return damage_c
 
 def damage_in():
-    if P_inxylanh < 0.8*P_in:
+    if P_inxylanh < 0.8*P_intake:
         damage_in = 'Xuppap bị kẹt (không mở hoàn toàn)'
-    elif P_inxylanh > 0.62*P_in:
+    elif P_inxylanh > 0.62*P_intake:
         damage_in = 'Lọt khí qua xecmang (xecmang đóng không kín)'
     return damage_in
     
