b97f999b8f795c831c2866ffe3e852178fcf12b1
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-11-14 16:11:42 +0700

    them

--- graph.py
+++ graph.py
@@ -51,6 +51,8 @@ class Ui_MainWindow(object):
         self.scene = QtWidgets.QGraphicsScene(self.graphicsView)
         self.graphicsView.setScene(self.scene)
         pos = [0,0]
+        pos_2 = [0,0]
+        pixel = [760,380]
         # Count number of values in list Filename 
         k = 0
         # Count value in list Filenames
@@ -58,26 +60,32 @@ class Ui_MainWindow(object):
         for file in filenames:
             img = QtGui.QImage(file)
             i += 1
-            k = int(len(filenames))
-            
-            if i <= round(k/2):
-                img = img.scaled(760/round(k/2),380/2)
+            k = float(len(filenames))
+            if k <2:
+                img = img.scaled(pixel[0], pixel[1])
                 Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
-                a = self.scene.addItem(Pimax_Item)
+                a = self.scene.addItem(Pimax_Item) 
+                self.graphicsView.fitInView(Pimax_Item)
+            elif k>=2:
                 
-                pos[0] += 765/round(k/2)
-                
-                Pimax_Item.setOffset(pos[0], pos[1])
-                
-            elif i > round(k/2):
-                pos[1] = 385/2
-                img = img.scaled(760/round(k/2),380/2)
-                Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
-                a = self.scene.addItem(Pimax_Item)
-                
-                Pimax_Item.setOffset(pos[0], pos[1])
-                
-                pos[0] -= 765/round(k/2)
+                if i <= round(k/2):        
+                    img = img.scaled(pixel[0]/round(k/2), pixel[1]/2)
+                    Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
+                    a = self.scene.addItem(Pimax_Item)
+                    
+                    Pimax_Item.setOffset(pos[0], pos[1])
+                    
+                    pos[0] += 765/round(k/2)
+                    
+                elif i > round(k/2):
+                    pos_2[1] = 385/2
+                    img = img.scaled(pixel[0]/(k-round(k/2)),pixel[1]/2)
+                    Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
+                    a = self.scene.addItem(Pimax_Item)
+                    
+                    Pimax_Item.setOffset(pos_2[0], pos_2[1])
+                    
+                    pos_2[0] += 765/(k-round(k/2))
                 
             
 
