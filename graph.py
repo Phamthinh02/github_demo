e1fed010493ce26ebb79857b4e3fd1a9314044d6
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-11-15 20:14:18 +0700

    them

--- graph.py
+++ graph.py
@@ -69,24 +69,18 @@ class Ui_MainWindow(object):
                 a = self.scene.addItem(Pimax_Item) 
                 self.graphicsView.fitInView(Pimax_Item)
             elif k>=2:
-                
                 if i <= round(k/2):        
                     img = img.scaled(pixel[0]/round(k/2), pixel[1]/2)
                     Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
                     a = self.scene.addItem(Pimax_Item)
-                    
-                    Pimax_Item.setOffset(pos[0], pos[1])
-                    
-                    pos[0] += 765/round(k/2)
-                    
+                    Pimax_Item.setOffset(pos[0], pos[1]) 
+                    pos[0] += 765/round(k/2) 
                 elif i > round(k/2):
                     pos_2[1] = 385/2
                     img = img.scaled(pixel[0]/(k-round(k/2)),pixel[1]/2)
                     Pimax_Item = QtWidgets.QGraphicsPixmapItem(QtGui.QPixmap.fromImage(img))
                     a = self.scene.addItem(Pimax_Item)
-                    
                     Pimax_Item.setOffset(pos_2[0], pos_2[1])
-                    
                     pos_2[0] += 765/(k-round(k/2))
                 
             
