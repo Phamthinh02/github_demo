4c20e6d78817df9dc3bdda06d9b8baef6edc0dc7
Thing Pham <90142229+Phamthinh02@users.noreply.github.com>
2021-12-02 20:30:59 +0700

    123

--- math/compare.py
+++ math/compare.py
@@ -1,29 +1,30 @@
 from analysis import *
 
 def normal():
-    if 0.8*Pc < Pmax < Pc and 0.8*P_intake < P_inxylanh < P_intake:
+    # Pmax lay gia ti lon nhat trong .dat , Pmin lay gia trinho nhat trong .dat
+    if 0.8*compression_pressure < Pmax < compression_pressure and 0.8*Minimum_pressure_intake < Pmin < Minimum_pressure_intake:
         value = 'Bình thường'
     else:
         value = 'Hư hỏng'
     return value
 
 def damage():
-    if Pci < P_min:
+    if Pci < Minimum_pressure:
         damage_c ='Gãy xéc măng, gãy xupap hay bị lủng piston'
-    elif P_min < Pci <0.62*Pmax:
+    elif Minimum_pressure < Pci <0.62*compression_pressure:
         damage_c = 'Hở gioăng nắp máy'
-    elif 0.62*Pc < Pci <0.8*Pc:
+    elif 0.62*compression_pressure < Pci <0.8*compression_pressure:
         damage_c = 'Hở xupap'
-    elif Pci > Pc:
+    elif Pci > compression_pressure:
         damage_c = 'Buồng đốt bị bám mụi than, kẹt xupap xả'
-    elif Pci > 0.9*Pc:
+    elif Pci > 0.9*compression_pressure:
         damage_c = 'Các xy lanh mòn không đều'
     return damage_c
 
 def damage_in():
-    if P_inxylanh < 0.8*P_intake:
+    if Pmin < 0.8*Minimum_pressure_intake:
         damage_in = 'Xuppap bị kẹt (không mở hoàn toàn)'
-    elif P_inxylanh > 0.62*P_intake:
+    elif Pmin > 0.62*P_iMinimum_pressure_intakentake:
         damage_in = 'Lọt khí qua xecmang (xecmang đóng không kín)'
     return damage_in
     
